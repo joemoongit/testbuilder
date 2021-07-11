eed1035431145ce0e7f198735dea46e483dc2e8b
joe <joemoondev@gmail.com>
2021-07-11 01:31:53 -0700

    Add tests for Discover and Maestro

--- detectNetwork.test.js
+++ detectNetwork.test.js
@@ -16,14 +16,14 @@ describe('Introduction to Mocha Tests - READ ME FIRST', function() {
   // Once you've read and understood this section, please comment it out.
   // You will not be able to proceed with a failing test.
 
-  it('Throws an error so it fails', function() {
-    throw new Error('Delete me!');
-  });
+  // it('Throws an error so it fails', function() {
+  //   throw new Error('Delete me!');
+  // });
 
   it('Doesn\'t throw an error, so it doesn\'t fail', function() {
     // This test doesn't really test anything at all! It will pass no matter what.
-    var even = function(num){
-      return num / 2 === 0;
+    var even = function(num) {
+      return num % 2 === 0;
     };
     return even(10) === true;
   });
@@ -31,11 +31,11 @@ describe('Introduction to Mocha Tests - READ ME FIRST', function() {
   // In tests, we want to compare the expected behavior to the actual behavior.
   // A test should only fail if the expected behavior doesn't match the actual.
   it('Throws an error when expected behavior does not match actual behavior', function() {
-    var even = function(num){
-      return num / 2 === 0;
+    var even = function(num) {
+      return num % 2 === 0;
     };
 
-    if(even(10) !== true) {
+    if (even(10) !== true) {
       throw new Error('10 should be even!');
     }
   });
@@ -44,14 +44,13 @@ describe('Diner\'s Club', function() {
   // Be careful, tests can have bugs too...
 
   it('has a prefix of 38 and a length of 14', function() {
-    throw new Error('Delete me!');
     if (detectNetwork('38345678901234') !== 'Diner\'s Club') {
       throw new Error('Test failed');
     }
   });
 
   it('has a prefix of 39 and a length of 14', function() {
-    if (detectNetwork('3934567890123') !== 'Diner\'s Club') {
+    if (detectNetwork('39345678901234') !== 'Diner\'s Club') {
       throw new Error('Test failed');
     }
   });
@@ -61,7 +60,7 @@ describe('American Express', function() {
   // It can get annoying to keep typing the if/throw, so here is a
   // helper function to throw an error if the input statement isn't true.
   var assert = function(isTrue) {
-    if (isTrue) {
+    if (!isTrue) {
       throw new Error('Test failed');
     }
   };
@@ -80,7 +79,7 @@ describe('Visa', function() {
   // Chai provides an assert that acts the same as our previous assert.
   // Search the documentation to figure out how to access it.
   //   http://chaijs.com/
-  var assert = chai.FILL_ME_IN;
+  var assert = chai.assert;
 
   it('has a prefix of 4 and a length of 13', function() {
     assert(detectNetwork('4123456789012') === 'Visa');
@@ -102,13 +101,13 @@ describe('MasterCard', function() {
   //   http://chaijs.com/api/bdd/
   var expect = chai.expect;
 
-  it(FILL_ME_IN, function() {
+  it('has a prefix of 51 and a length of 16', function() {
     expect(detectNetwork('5112345678901234')).to.equal('MasterCard');
   });
-  it(FILL_ME_IN, function() {
+  it('has a prefix of 52 and a length of 16', function() {
     expect(detectNetwork('5212345678901234')).to.equal('MasterCard');
   });
-  it(FILL_ME_IN, function() {
+  it('has a prefix of 53 and a length of 16', function() {
     expect(detectNetwork('5312345678901234')).to.equal('MasterCard');
   });
 
@@ -122,22 +121,61 @@ describe('MasterCard', function() {
   var should = chai.should();
 
   it('has a prefix of 54 and a length of 16', function() {
-    detectNetwork('5412345678901234').should.equal(FILL_ME_IN);
+    detectNetwork('5412345678901234').should.equal('MasterCard');
   });
   it('has a prefix of 55 and a length of 16', function() {
-    detectNetwork('5512345678901234').should.equal(FILL_ME_IN);
+    detectNetwork('5512345678901234').should.equal('MasterCard');
   });
 });
 
 describe('Discover', function() {
   // Tests without a function will be marked as "pending" and not run
   // Implement these tests (and others) and make them pass!
-  it('has a prefix of 6011 and a length of 16');
-  it('has a prefix of 6011 and a length of 19');
+  var assert = chai.assert;
+
+  var helper = function(prefix, number, card) {
+    return it('has a prefix of ' + prefix + ' and a length of ' + number.length, function() {
+      assert(detectNetwork(number) === card);
+    });
+  };
+
+  var tests = [
+    {prefix: '6011', number: '6011594875624584', card: 'Discover'},
+    {prefix: '6011', number: '6011594875624584123', card: 'Discover'},
+    {prefix: '644', number: '6441594875624584', card: 'Discover'},
+    {prefix: '645', number: '6451594875624584123', card: 'Discover'},
+    {prefix: '646', number: '6461594875624584', card: 'Discover'},
+    {prefix: '647', number: '6471594875624584123', card: 'Discover'},
+    {prefix: '648', number: '6481594875624584', card: 'Discover'},
+    {prefix: '649', number: '6491594875624584123', card: 'Discover'},
+    {prefix: '65', number: '6541594875624584', card: 'Discover'}
+  ];
+
+  tests.forEach(function(test) { helper(test.prefix, test.number, test.card); });
 });
 
 describe('Maestro', function() {
   // Write full test coverage for the Maestro card
+  var assert = chai.assert;
+
+  var helper = function(prefix, number, card) {
+    return it('has a prefix of ' + prefix + ' and a length of ' + number.length, function() {
+      assert(detectNetwork(number) === card);
+    });
+  };
+
+  var tests = [
+    {prefix: '5018', number: '501875624584', card: 'Maestro'},
+    {prefix: '5020', number: '5020756245841', card: 'Maestro'},
+    {prefix: '5038', number: '50387562458412', card: 'Maestro'},
+    {prefix: '6304', number: '630475624584123', card: 'Maestro'},
+    {prefix: '5018', number: '5018756245841234', card: 'Maestro'},
+    {prefix: '5020', number: '50207562458412345', card: 'Maestro'},
+    {prefix: '5038', number: '503875624584123456', card: 'Maestro'},
+    {prefix: '6304', number: '6304756245841234567', card: 'Maestro'}
+  ];
+
+  tests.forEach(function(test) { helper(test.prefix, test.number, test.card); });
 });
 
 
