e0855bf03903f9184bf724ade3c89ab566001288
joe <joemoondev@gmail.com>
2021-07-11 02:30:20 -0700

    Passes all test cases

--- detectNetwork.js
+++ detectNetwork.js
@@ -10,38 +10,39 @@
 // Warning: Regular Expressions (RegEx) are NOT ALLOWED on this assignment!
 
 var detectNetwork = function(cardNumber) {
-  // Note: `cardNumber` will always be a string
-  // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
-  // The American Express network always starts with a 34 or 37 and is 15 digits long
 
-  var length = cardNumber.length;
-  var prefix = cardNumber[0];
-  var prefix2 = cardNumber[1];
-  if (length === 13) {
-    if (prefix === '4') {
-      return 'Visa';
+  var cards = [
+    {name: 'Diner\'s Club', prefix: ['38', '39'], lengths: [14]},
+    {name: 'American Express', prefix: ['34', '37'], lengths: [15]},
+    {name: 'Visa', prefix: ['4'], lengths: [13, 16, 19]},
+    {name: 'MasterCard', prefix: ['51', '52', '53', '54', '55'], lengths: [16]},
+    {
+      name: 'Discover',
+      prefix: ['6011', '644', '645', '646', '647', '648', '649', '65'],
+      lengths: [16, 19]
+    },
+    {
+      name: 'Maestro',
+      prefix: ['5018', '5020', '5038', '6304'],
+      lengths: [12, 13, 14, 15, 16, 17, 18, 19]
     }
-  } else if (length === 14) {
-    if (prefix === '3' && (prefix2 === '8' || prefix2 === '9')) {
-      return 'Diner\'s Club';
-    }
-  } else if (length === 15) {
-    if (prefix === '3' && (prefix2 === '4' || prefix2 === '7')) {
-      return 'American Express';
-    }
-  } else if (length === 16) {
-    if (prefix === '4') {
-      return 'Visa';
-    } else if (prefix === '5' && (['1', '2', '3', '4', '5'].includes(prefix2))) {
-      return 'MasterCard';
-    }
-  } else if (length === 19) {
-    if (prefix === '4') {
-      return 'Visa';
-    }
-  }
+  ];
 
-  // Once you've read this, go ahead and try to implement this function, then return to the console.
+  var res = '';
+
+  cards.forEach(
+    function(card) {
+      card.prefix.forEach(
+        function(pre) {
+          if (pre === cardNumber.slice(0, pre.length)) {
+            res = card.name;
+          }
+        }
+      );
+    }
+  );
+
+  return res;
 };
 
 
