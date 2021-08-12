b7f15a7b29db279efbac35508a4a7ec43b5f60e3
Joseph Moon <joemoondev@gmail.com>
2021-08-11 20:56:29 -0700

    Cleaned up a bit

--- detectNetwork.test.js
+++ detectNetwork.test.js
@@ -2,30 +2,30 @@
  * Tests for detectNetwork.js
  *
  */
+var should = chai.should();
+
+var helper = function(prefix, number, card) {
+  return it('has a prefix of ' + prefix + ' and a length of ' + number.length, function() {
+    detectNetwork(number).should.equal(card);
+  });
+};
+
+var generateNumber = function(prefix, length) {
+  var res = prefix;
+  if (res.length !== length) {
+    var numDigits = Math.pow(10, length - res.length - 1);
+    res += String(Math.floor(numDigits + Math.random() * (9 * numDigits)));
+  }
+  return res;
+};
+var generateTest = function(p, n) {
+  return { prefix: p, number: n, card: card.name };
+};
+
+var tests = [];
 
 var test = function(card) {
   describe(card.name, function() {
-    var should = chai.should();
-
-    var helper = function(prefix, number, card) {
-      return it('has a prefix of ' + prefix + ' and a length of ' + number.length, function() {
-        detectNetwork(number).should.equal(card);
-      });
-    };
-
-    var generateNumber = function(prefix, length) {
-      var res = prefix;
-      if (res.length !== length) {
-        var numDigits = Math.pow(10, length - res.length - 1);
-        res += String(Math.floor(numDigits + Math.random() * (9 * numDigits)));
-      }
-      return res;
-    };
-    var generateTest = function(p, n) {
-      return { prefix: p, number: n, card: card.name };
-    };
-
-    var tests = [];
     var addTests = function(prefixes, lengths) {
       for (var i = 0; i < prefixes.length; i++) {
         var prefix = prefixes[i];
